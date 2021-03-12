0e153565d56cb2daa8fb61ebbb9e102363506e30
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-03-12 08:59:21 +0100

    refactoring and architecture implementation

--- script.js
+++ script.js
@@ -11,14 +11,74 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { longitude, latitude } = position.coords;
-    console.log(position);
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-  },
+class App {
+  #map;
+  #mapEvent;
 
-  function () {
-    alert("could not get your position");
+  constructor() {
+    this._getPosition();
+
+    form.addEventListener(`submit`, this._newWorkout.bind(this));
+
+    inputType.addEventListener(`change`, this._toggleElavationField);
   }
-);
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("could not get your position");
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { longitude } = position.coords;
+    const { latitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on(`click`, this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleElavationField() {
+    inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+    inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    //clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = ``;
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `running-popup`,
+        })
+      )
+      .setPopupContent(`Workout`)
+      .openPopup();
+  }
+}
+
+const app = new App();
